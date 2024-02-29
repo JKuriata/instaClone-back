55967d9dba91c128cef78c5c0cd84f5b6de7bc7e
JKuriata <JK271096@outlook.com>
2024-02-29 11:42:54 +0000

    chore: setup signin

---
+++ src/users/controllers.js
@@ -0,0 +1,28 @@
+const User = require("./model");
+
+const signupUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    res.status(201).json({ message: "user added", user: user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    res.send({ messege: "Login successful", user: req.user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+
+module.exports = {
+  signupUser: signupUser,
+};
